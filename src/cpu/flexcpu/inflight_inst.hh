5f97fecc0235f8cd14e3f3e54454e650485327da
Bradley Wang <radwang@ucdavis.edu>
2018-10-08 19:18:23 -0700

    cpu-flex: in-order/in-order-superscalar parameters
    Change-Id: I5a557a668300efae70b2ed8a839b6a5df62247ab
    Signed-off-by: Bradley Wang <radwang@ucdavis.edu>

--- src/cpu/flexcpu/inflight_inst.hh
+++ src/cpu/flexcpu/inflight_inst.hh
@@ -159,6 +159,7 @@ class InflightInst : public ExecContext,
     TimingRecord _timingRecord;
 
     // Callbacks made during a state transition.
+    std::vector<std::function<void()>> beginExecCallbacks;
     std::vector<std::function<void()>> commitCallbacks;
     std::vector<std::function<void()>> completionCallbacks;
 
@@ -211,6 +212,8 @@ class InflightInst : public ExecContext,
 
     virtual ~InflightInst();
 
+    void addBeginExecCallback(std::function<void()> callback);
+    void addBeginExecDependency(InflightInst& parent);
     /**
      * Tells this InflightInst to call a particular function when it has been
      * made aware of a commit.
@@ -308,6 +311,9 @@ class InflightInst : public ExecContext,
 
     inline bool isEffAddred() const
     { return status() >= EffAddred; }
+
+    inline bool isExecuting() const
+    { return status() >= Executing; }
 
     inline bool isFaulted() const
     { return fault() != NoFault; }
